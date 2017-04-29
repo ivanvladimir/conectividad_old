11b80bff20f6560db19ccb14d81fbfacbf5fd445
penserbjorne <penserbjorne@users.noreply.github.com>
2017-04-29 16:00:30 -0500

    Se comprimio el codigo y se comento

--- src/python/module_canonical_name.py
+++ src/python/module_canonical_name.py
@@ -19,6 +19,27 @@ import datetime
 from tinydb import TinyDB, Query
 import re
 
+#   Update node in contensiosos
+def update_contenciosos(contensiosos, case, court, involved, name, actions, date_sentence, number, error):
+    contensiosos.update(  # Actualizamos
+        {
+            'date_modification': datetime.datetime.now().isoformat(' '),
+            'meta_name': {
+                'court': court,
+                'involved': {
+                    'involved_a': involved[0],
+                    'involved_b': involved[1]
+                },
+                'name': name,
+                'actions': actions,
+                'date_sentence': date_sentence,
+                'number': number,
+                'error' : error
+            }
+        },
+        eids=[case.eid]
+    )
+    return
 
 # MAIN
 if __name__ == "__main__":
@@ -42,95 +63,72 @@ if __name__ == "__main__":
        def verbose(*args):
             print("".join([str(x) for x in args]),file=sys.stderr)
     else:   
-        verbose = lambda *a: None  
+        verbose = lambda *a: None
 
+    #   Connecting to database
     verbose("Connecting to DB:",args.dbname)
     db = TinyDB(args.dbname)
+    #   Selecting table
     contensiosos = db.table('contensiosos')
 
     error_counter = 0
+
+    # Begin for
     for case in contensiosos.all():
         title = case['title']
         verbose('-->\nExtracting data from : ',title)
         meta = re.split('(?<!Vs|No)\. ', title)
         verbose(meta)
-        # Data fields
         court = ""
         involved = ""
         name = ""
         actions = ""
         date_sentence = ""
         number = ""
+        error = "No"
+        #   Try to get metadata
         try:
             court = meta[0]
-            involved = re.split(' Vs\. ',meta[1])
-            involved[0] = re.sub('(Caso )', '', involved[0])
+            involved = re.split(' Vs\. ',meta[1])   # Separamos la cadena en dos, por ahora no hay mas involucrados
+            involved[0] = re.sub('(Caso )', '', involved[0])    #   Retiramos la palabra Caso del inicio
+                                                                # no se han presentado otras palabras a retirar
             name = meta[1]
             actions = meta[2]
-            date_sentence = re.search('\d{1,2}[a-zA-Z ]*\d{4}',meta[3]).group(0)
-            number = meta[4][:len(meta[4]) - 1]
-            contensiosos.update(
-                {
-                    'date_modification':datetime.datetime.now().isoformat(' '),
-                    'meta_name': {
-                        'court': court,
-                        'involved': {
-                            'involved_a': involved[0],
-                            'involved_b': involved[1]
-                        },
-                        'name': name,
-                        'actions' : actions,
-                        'date_sentence': date_sentence,
-                        'number': number
-                    }
-                },
-                eids=[case.eid]
-            )
+            date_sentence = re.search('\d{1,2}[a-zA-Z ]*\d{4}',meta[3]).group(0)    #   Obtenemos solamente la fecha
+            number = meta[4][:len(meta[4]) - 1] #   Retiramos el punto del final, no pertenece al dato.
+            error = "No"
+            update_contenciosos(contensiosos, case, court, involved, name, actions, date_sentence, number, error)
+        #   End try
+        #   Begin except
         except:
-            contensiosos.update(
-                {
-                    'date_modification': datetime.datetime.now().isoformat(' '),
-                    'meta_name': {
-                        'court': "N/A",
-                        'involved': {
-                            'involved_a': "N/A",
-                            'involved_b': "N/A"
-                        },
-                        'name': "N/A",
-                        'actions': "N/A",
-                        'date_sentence': "N/A",
-                        'number': "N/A"
-                    }
-                },
-                eids=[case.eid]
-            )
             message = "**********Some error just happened :(*********\nPlease verify the partitioned data :/"
             error_counter += 1
+            partitioned_data = \
+                '\tcourt: %s\n' \
+                '\tinvolved: %s\n' \
+                '\tname: %s\n' \
+                '\tactions: %s\n' \
+                '\tdate_sentence: %s\n' \
+                '\tnumber: %s'
             if args.interactive:
-                partitioned_data = \
-                    '\tcourt: %s\n' \
-                    '\tinvolved: %s\n' \
-                    '\tname: %s\n' \
-                    '\tactions: %s\n' \
-                    '\tdate_sentence: %s\n' \
-                    '\tnumber: %s'
+                print(message)
                 if not args.verbose:
-                    print(message)
-                    print(partitioned_data % (court, involved, name, actions, date_sentence, number))
-                else:
-                    verbose(message)
-                    verbose(partitioned_data % (court, involved, name, actions, date_sentence, number))
+                    print (meta)
+                print(partitioned_data % (court, involved, name, actions, date_sentence, number))
                 input("Press Enter to continue...")
-
-
-        partitioned_data = \
-            '\tcourt: %s\n' \
-            '\tinvolved: %s\n' \
-            '\tname: %s\n' \
-            '\tactions: %s\n' \
-            '\tdate_sentence: %s\n' \
-            '\tnumber: %s'
-        if not args.interactive:
-            verbose(partitioned_data % (court, involved, name, actions, date_sentence, number))
+            elif args.verbose:
+                verbose(message)
+                verbose(partitioned_data % (court, involved, name, actions, date_sentence, number))
+            court = "N\A"
+            involved[0] = "N\A"
+            involved.append("N\A")
+            name = "N\A"
+            actions = "N\A"
+            date_sentence = "N\A"
+            number = "N\A"
+            error = "Yes"
+            update_contenciosos(contensiosos, case, court, involved, name, actions, date_sentence, number, error)
+        #   End except
+    #   End for
 
     print("Errors: %s" % error_counter)
