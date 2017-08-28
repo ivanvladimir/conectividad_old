7f3f6cc38949c8b53047d8993d63cdb14a109c4d
Penserbjorne <paul.aguilar.enriquez@hotmail.com>
2017-08-27 23:18:50 -0500

    Se modifico el modulo module_canonica_name.py , por ahora ya no hay extraccion con errores. @ivanvladimir ya puedes correrlo para actualizar tu DB.json

--- src/python/module_canonical_name.py
+++ src/python/module_canonical_name.py
@@ -77,25 +77,78 @@ if __name__ == "__main__":
     for case in contensiosos.all():
         title = case['title']
         verbose('-->\nExtracting data from : ',title)
-        meta = re.split('(?<!Vs|No)\. ', title)
+        meta = re.split('(?<!(Vs|No))\. ', title)
         verbose(meta)
-        court = ""
-        involved = ""
-        name = ""
-        actions = ""
-        date_sentence = ""
-        number = ""
-        error = "No"
-        #   Try to get metadata
+        court = "N\A"
+        involved = []
+        involved.append("N\A")
+        involved.append("N\A")
+        name = "N\A"
+        actions = "N\A"
+        date_sentence = "N\A"
+        number = "N\A"
+        error = "Yes"
+        #   Try to get metadata , this should break ;@ need a better method, sorry ;@
         try:
-            court = meta[0]
-            involved = re.split(' Vs\. ',meta[1])   # Separamos la cadena en dos, por ahora no hay mas involucrados
-            involved[0] = re.sub('(Caso )', '', involved[0])    #   Retiramos la palabra Caso del inicio
-                                                                # no se han presentado otras palabras a retirar
-            name = meta[1]
-            actions = meta[2]
-            date_sentence = re.search('\d{1,2}[a-zA-Z ]*\d{4}',meta[3]).group(0)    #   Obtenemos solamente la fecha
-            number = meta[4][:len(meta[4]) - 1] #   Retiramos el punto del final, no pertenece al dato.
+            if len(meta) == 11:
+                # Vs. separado
+                court = meta[0]
+                name = meta[2] + ". " + meta[4]
+                involved[0] = re.sub('(Caso )', '', meta[2]) + "."  # Retiramos la palabra Caso del inicio
+                                                                    # no se han presentado otras palabras a retirar
+                involved[1] = re.sub('(Vs\. )', '', meta[4])  # Retiramos la palabra Vs. del inicio
+                actions = meta[6]
+                date_sentence = re.search('\d{1,2}[a-zA-Z ]*\d{4}', meta[8]).group(0)  # Obtenemos solamente la fecha
+                number = meta[10][:len(meta[10]) - 1]  # Retiramos el punto del final, no pertenece al dato.
+            elif len(meta) == 9:
+                if meta[8].__contains__("Sentencia") and meta[8].__contains__("Serie"):
+                    # Vs. separado pero ...
+                    # ... no existe punto final/separador en la fecha de la sentencia
+                    court = meta[0]
+                    name = meta[2] + ". " + meta[4]
+                    involved[0] = re.sub('(Caso )', '', meta[2]) + "."  # Retiramos la palabra Caso del inicio
+                    # no se han presentado otras palabras a retirar
+                    involved[1] = re.sub('(Vs\. )', '', meta[4])  # Retiramos la palabra Vs. del inicio
+                    actions = meta[6]
+
+                    temp = re.split(' Serie ', meta[8])  # Separamos la cadena en dos
+                    date_sentence = re.search('\d{1,2}[a-zA-Z ]*\d{4}', temp[0]).group(0)  # Obtenemos solamente la fecha
+                    number = "Serie " + temp[1][:len(temp[1]) - 1]  # Retiramos el punto del final, no pertenece al dato.
+                else:
+                    # Vs. junto
+                    court = meta[0]
+                    name = meta[2]
+                    involved = re.split(' Vs\. ', meta[2])  # Separamos la cadena en dos, por ahora no hay mas involucrados
+                    involved[0] = re.sub('(Caso )', '', involved[0])  # Retiramos la palabra Caso del inicio
+                                                                        # no se han presentado otras palabras a retirar
+                    actions = meta[4]
+                    date_sentence = re.search('\d{1,2}[a-zA-Z ]*\d{4}', meta[6]).group(0)  # Obtenemos solamente la fecha
+                    number = meta[8][:len(meta[8]) - 1]  # Retiramos el punto del final, no pertenece al dato.
+            elif len(meta) == 7:
+                if meta[6].__contains__("Sentencia") and meta[6].__contains__("Serie"):
+                    # Vs. Junto y con acciones, pero ...
+                    # ... no existe punto final/separador en la fecha de la sentencia
+
+                    court = meta[0]
+                    name = meta[2]
+                    involved = re.split(' Vs\. ', meta[2])  # Separamos la cadena en dos, por ahora no hay mas involucrados
+                    involved[0] = re.sub('(Caso )', '', involved[0])  # Retiramos la palabra Caso del inicio
+                                                                        # no se han presentado otras palabras a retirar
+                    actions = meta[4]
+                    temp = re.split(' Serie ',meta[6])  # Separamos la cadena en dos
+                    date_sentence = re.search('\d{1,2}[a-zA-Z ]*\d{4}', temp[0]).group(0)  # Obtenemos solamente la fecha
+                    number = "Serie " + temp[1][:len(temp[1]) - 1]  # Retiramos el punto del final, no pertenece al dato.
+                else:
+                    # Vs. junto y sin acciones
+                    court = meta[0]
+                    name = meta[2]
+                    involved = re.split(' Vs\. ', meta[2])  # Separamos la cadena en dos, por ahora no hay mas involucrados
+                    involved[0] = re.sub('(Caso )', '', involved[0])  # Retiramos la palabra Caso del inicio
+                                                                        # no se han presentado otras palabras a retirar
+                    actions = "No especificado."
+                    date_sentence = re.search('\d{1,2}[a-zA-Z ]*\d{4}', meta[4]).group(0)  # Obtenemos solamente la fecha
+                    number = meta[6][:len(meta[6]) - 1]  # Retiramos el punto del final, no pertenece al dato.
+
             error = "No"
             update_contenciosos(contensiosos, case, court, involved, name, actions, date_sentence, number, error)
         #   End try
@@ -119,14 +172,6 @@ if __name__ == "__main__":
             elif args.verbose:
                 verbose(message)
                 verbose(partitioned_data % (court, involved, name, actions, date_sentence, number))
-            court = "N\A"
-            involved[0] = "N\A"
-            involved.append("N\A")
-            name = "N\A"
-            actions = "N\A"
-            date_sentence = "N\A"
-            number = "N\A"
-            error = "Yes"
             update_contenciosos(contensiosos, case, court, involved, name, actions, date_sentence, number, error)
         #   End except
     #   End for
