781789e175d43b2892bd4869bd100e893b7de800
Ivan Vladimir Meza Ruiz <ivanvladimir@gmail.com>
2017-11-05 04:50:27 -0600

    Fixes some extraction

--- src/python/extract_articles.py
+++ src/python/extract_articles.py
@@ -26,39 +26,47 @@ from bs4 import BeautifulSoup
 # local libraries
 from utils import pdf2text, extract_topics, search_regexp
 
+from string import punctuation
+non_words = list(punctuation)                                                    
+#we add spanish punctuation                                                      
+non_words.extend(['¿', '¡'])                                                     
+non_words.extend(map(str,range(10)))  
+
 sws=stopwords.words('spanish')
 
 
 
 
 re_articulo=re.compile('[\d\.]+')
-re_articulos=re.compile('art[íi]culos?')
-re_recovery=re.compile('(artículos?|articulos?) (?P<articles>[^;]+?) (de esa|de esta|de la |del |de su |en la )?(?P<source>(dich[oa] |últim[oa] |presente ley |ley )?[^"",;.()]*)[,.;]?')
-re_numbers=re.compile('[\d.]+')
-re_mismo=re.compile('(últim[ao]|mism[oa]|dich[oa]|presente|esta ley|([A-Z][^ \d]+ *([A-Z][^ \d]+ *|para +|do +|de +|en +|las? +|del +|sobre +|en +|contra +|los? +|\[\w+\][ .,;]+)*)\d*)')
+re_articulos=re.compile('art.culos?')
+re_recovery=re.compile('(art.culos?|art.culos?) (?P<articles>[\d.,y ixviabc]+-?) (fracción [^,]*, )?(inciso [^,]*, )?(de esa|de esta|de la |del |de su |en la )?(?P<source>(dich[oa] |últim[oa] |presente ley |ley )?[^",;.()]*)[,.;]?')
+re_numbers=re.compile('([\d.]+|[ixv]+)')
+re_mismo=re.compile('(últim[ao]|mism[oa]|dicha Convención|dich[oa]|presente|esta ley|([A-Z][^ \d]+ *([A-Z][^ \d]+ *|\d +|mexicana|por +|al +|para +|do +|de +|en +|las? +|del +|sobre +|en +|contra +|los? +|\[\w+\][ .,;]+)*)\d*)')
 re_spaces=re.compile("\s+")
-re_en_adelante=re.compile('([A-Z]\w*( [A-Z]\w*| para| y| contra| Prevenir,| Educación, | sobre| de| la| y| el)*) \( ?en *adelante,? *(?:también *)?"([^"]+)"(?: o "([^"]+)")?')
-
+re_en_adelante=re.compile('([A-Z]\w*( [A-Z]\w*| para| y| contra| Prevenir,| Educación, | sobre| del| en| de| la| los| y| el)*) \( ?en *adelante,? *(?:también *)?"([^"]+)"(?: o "([^"]+)")?')
+re_corchetes=re.compile('([A-Z]\w*) \[(.*)\]')
+re_anafora=re.compile("[A-Z]\w*( de| [A-Z]\w*)*")
 
 reductions=[
 (re.compile('mism[ao]'),'PENDING'),
 (re.compile('MISM[AO]'),'PENDING'),
+(re.compile('dicha Convención'),'Convención'),
 (re.compile('dich[oa]'),'PENDING'),
 (re.compile('presente'),'PENDING'),
 (re.compile('esta ley'),'PENDING'),
-(re.compile('Ley'),'PENDING'),
+(re.compile('Son'),'PENDING'),
 (re.compile('est[ae] últim[oa]'),'PENDING'),
-(re.compile('Convención [I|i]'),'Convención Interamericana de Derechos Humanos'),
-(re.compile('Convención [A|a]'),'Convención Americana sobre Derechos Humanos'),
+(re.compile('Convención [I|i]\w*'),'Convención Interamericana de Derechos Humanos'),
+(re.compile('Convención [A|a]\w*'),'Convención Americana sobre Derechos Humanos'),
 (re.compile('RAAN'),'Regiones Autónomas del Atlántico Norte'),
 (re.compile('MARENA'),'Ministerio del Ambiente y Recursos Naturales de Nicaragua'),
 (re.compile('RAAS'),'Regiones Autónomas del Atlántico Sur'),
-(re.compile('CONVENCIÓN [I|i]'),'Convención Interamericana de Derechos Humanos'),
-(re.compile('CONVENCIÓN [A|i]'),'Convención Americana de Derechos Humanos'),
+(re.compile('CONVENCIÓN [I|i]\w*'),'Convención Interamericana de Derechos Humanos'),
+(re.compile('CONVENCIÓN [A|a]\w*'),'Convención Americana de Derechos Humanos'),
 (re.compile('Estatuto.*(corte)*'),'Estatuto de la Corte'),
 (re.compile('Reglamento.*(corte)*'),'Reglamento de la Corte'),
 (re.compile('CP'),'Código Penal'),
-(re.compile('Código'),'Código Penal'),
+(re.compile('CIDFP'),'Convención Interamericana sobre Desaparición Forzada de Personas'),
 ]
 
 
@@ -120,7 +128,6 @@ if __name__ == "__main__":
     db = TinyDB(args.dbname)
     contensiosos = db.table('contensiosos')
 
-    print(len(contensiosos))
 
 
     re_selector=re.compile(args.re_selector)
@@ -139,15 +146,19 @@ if __name__ == "__main__":
             verbose(bcolors.FAIL +'ARCHIVO FALTANTE '+bcolors.ENDC,xmlfile)
             continue
         prev_source=None
+        ##definitions={}
         definitions={'convención':"Convención Americana sobre Derechos Humanos"}
         for par in soup.select('paragraph'):
             text=par.text.replace("“",'"').replace("”",'"').replace("No.",'Num')
             text=text.replace("ó",'ó')
             text=text.replace('"la Convención Americana o','"la Convención Americana" o')
+            text=text.replace('Proyecto de Artículo','Proyecto de Art')
+            text=text.replace('Convención26','Convención')
             sentences = sent_tokenize(text)
             for sentence in sentences:
                 sentence_original=sentence.replace('\n',' ')
                 sentence_original=re_spaces.sub(' ',sentence_original)
+                sentence_original=sentence_original.replace('protección de todas las personas contra las desapariciones forzadas','Protección de Todas las Personas contra las Desapariciones Forzadas')
                 verbose(bcolors.OKBLUE +'> Sentence '+ bcolors.ENDC ,sentence_original)
                 sentence_lower=sentence_original.lower()
                 m=re_articulo.search(sentence_lower)
@@ -160,7 +171,9 @@ if __name__ == "__main__":
                         if m.group(4):
                             definitions[m.group(4).lower().replace('la ','').replace('el ','').strip()+" "]=m.group(1) 
                             verbose(bcolors.HEADER +'> Definition '+ bcolors.ENDC,m.group(4),'to',m.group(1))
-
+                    for m in re_corchetes.finditer(sentence_original):
+                        definitions[m.group(1).lower()]=m.group(1)+" "+m.group(2) 
+                        verbose(bcolors.HEADER +'> Definition '+ bcolors.ENDC,m.group(1)+" "+m.group(2),'to',m.group(1))
 
 
                     for m in re_articulos.finditer(sentence_lower):
@@ -169,6 +182,12 @@ if __name__ == "__main__":
                     starts.append(len(sentence_lower))
 
                     segs=zip(starts[:-1],starts[1:])
+                    if len(starts)<=1:
+                        for m in re_anafora.finditer(sentence_original):
+                            verbose(bcolors.FAIL +'> ANAFORA '+ bcolors.ENDC,m.group(0))
+                            anafora=m.group(0)
+
+
                     for ini,fin in segs:
                         bit=sentence_lower[ini:fin]
                        
@@ -176,6 +195,7 @@ if __name__ == "__main__":
                         if not m:
                             continue
                         verbose(bcolors.WARNING +'> BIT '+ bcolors.ENDC,bit)
+                        print(m.groups())
                         source_start,source_end=m.span('source')
                         SOURCE=sentence_original[ini+source_start:ini+source_end].strip()
                         mismo=re_mismo.search(SOURCE)
@@ -183,22 +203,28 @@ if __name__ == "__main__":
                         if not mismo:
                             continue
                         source=mismo.group(1).strip()
+                        print(mismo.groups())
                      
-                        for definition in definitions.keys():
-                            #print(source.lower().strip().find(definition),definition,source.lower().strip())
-                            if (source.lower().strip()+" ").find(definition)>=0:
-                                #print("HIT HIT",source,definition,definitions)
-                                source=definitions[definition]
-                                break
-
+                        definitions_=[(len(k),k) for k in definitions.keys()]
+                        definitions_.sort()
+                        definitions_.reverse()
                         for re_, source_ in reductions:
                             if re_.search(source):
                                 source=source_
                                 break
 
+                        for _,definition in definitions_:
+                            #print(source.lower().strip().find(definition),definition,source.lower().strip())
+                            if (source.lower().strip()+" ").find(definition)==0:
+                                source=definitions[definition]
+                                break
+
+                        
                         if source.startswith('PENDING'):
                             if prev_source:
                                 source=prev_source
+                            elif len(anafora.split())>1:
+                                source=anafora
                             else:
                                 verbose(bcolors.FAIL +'> PREV_SOURCE '+ bcolors.ENDC,SOURCE)
 
@@ -209,8 +235,12 @@ if __name__ == "__main__":
                         articles=re_numbers.findall(arts)
                         if len(articles)==0:
                             continue
-                        if source in ["de",'en','del','los','II','El','Las','La','Los']:
+                            prev_source=None
+                        if source in ["de",'en','del','los','II','El','Las','La','Los',"De"]:
                             continue
+                            prev_source=None
+
+                        source = ''.join([c for c in source if c not in non_words])
                         verbose(bcolors.ORANGE +'> SOURCE '+ bcolors.ENDC,source)
                         verbose(bcolors.ORANGE +'> ARTICLES '+ bcolors.ENDC,articles)
                         for article in articles:
@@ -237,7 +267,7 @@ id2node={}
 
 re_pais=re.compile(r".*\.([^.]*)$")
 for idd,k in  enumerate(hist_sources.keys()):
-    if hist_sources[k]>3:
+    if hist_sources[k]>4:
         case=hist_sources_[k]
         m=re_pais.match(case['meta_name']['name'])
         pais="unknown"
@@ -248,7 +278,7 @@ for idd,k in  enumerate(hist_sources.keys()):
 
 nnode=len(contensiosos)+1
 for idd,k in  enumerate(hist_dest.keys()):
-    if hist_dest[k]>0:
+    if hist_dest[k]>4:
         JSON['nodes'].append({"id":nnode,"type":2,"name":k})
         name2id[k]=nnode
         nnode+=1
